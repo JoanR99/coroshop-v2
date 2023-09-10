cb3ff3d087a314149a9abccc23a9bf2e23ad427d
Joan Romero <romerojoan1999@gmail.com>
2023-09-10 19:32:32 -0400

    dashboard app setup

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'ui';
+import App from './app/App.tsx';
+import './index.css';
+import { store } from './app/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<Router>
+					<Routes>
+						<Route path="/*" element={<App />} />
+					</Routes>
+				</Router>
+			</ThemeProvider>
+		</Provider>
+	</React.StrictMode>
+);
